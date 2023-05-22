8c6a2486ca2d732496d11e20b728c085ac096a5d
Aman Kumar Gupta <amangupta15jan@gmail.com>
2023-05-22 19:56:52 +0530

    UPDATED: ecosytem file to cjs to support common js

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,6 +2,10 @@ module.exports = {
   apps: [
     {
       script: "./app.mjs",
+      instances: 1,
+      autorestart: true,
+      watch: true,
+      max_memory_restart: "1G",
       env_stage: {
         name: "AMAN_PORTFOLIO_STAGE-3000",
         NODE_ENV: "staging",
